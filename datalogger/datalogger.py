929210d85b6bb68c3029f6b455691142ee237863
jonathandb <jonathan.de.beir@gmail.com>
2014-04-04 09:30:46 -0300

    cleaned up code

--- datalogger/datalogger.py
+++ datalogger/datalogger.py
@@ -48,29 +48,26 @@ class DataLogger:
         try:
             # initiate logger
             self.logger = logging.getLogger()
-
             self.logger.setLevel(logging.DEBUG)
-
             self.log_send_store_handler = LogSendStoreHandler(LOG_LOCATION)
-
             formatter = logging.Formatter(
                 '%(asctime)s - %(levelname)s - %(name)s - %(message)s')
             self.log_send_store_handler.setFormatter(formatter)
-
             self.logger.addHandler(self.log_send_store_handler)
-
             self.logger.info('Initialising system...')
+            job_info_filter = JobInfoFilter()
+            logging.getLogger('apscheduler.scheduler').addFilter(
+                job_info_filter)
+            logging.getLogger('apscheduler.threadpool').addFilter(
+                job_info_filter)
+
             # load local configuration
             self.conf_man = ConfigurationManager(CONFIG_LOCATION)
             self.log_send_store_handler.update_configuration()
 
             self.scheduler = Scheduler()
             self.scheduler.start()
-            job_info_filter = JobInfoFilter()
-            logging.getLogger('apscheduler.scheduler').addFilter(
-                job_info_filter)
-            logging.getLogger('apscheduler.threadpool').addFilter(
-                job_info_filter)
+
             self.packet_manager = PacketManager(self.scheduler)
 
             # initiate network connection
@@ -85,7 +82,6 @@ class DataLogger:
             connected = self.connection.check_internet_connection(
                 ) and self.connection.check_server_connection()
             if connected:
-
                 self.load_online_configuration_and_initiate_sending_data()
                 self.packet_manager.initiate_send_packets(self.connection)
             else:
@@ -95,7 +91,6 @@ class DataLogger:
                     temporarily use offline timer and modbus slave
                     configuration
                 '''
-
                 self.wait_for_connection_to_load_configuration()
 
             # initiate sensor timers
@@ -106,7 +101,7 @@ class DataLogger:
             self.led_manager.update_led(PinName.powered, LedState.on)
             self.set_up_led_manager_calls()
 
-            # sleep 1 second to intialise led of log handler
+            # sleep 2 seconds to intialise led of log handler
             sleep(1)
             self.logger.info('Initialisation complete')
 
@@ -137,7 +132,6 @@ class DataLogger:
             if self.conf_man.is_online_configuration_different(online_checksum):
                 self.logger.info(
                     'Online configuration is new, updating configuration..')
-                # online configuration is different
                 online_configuration = self.connection.get_configuration()
                 self.conf_man.validate_json_configuration(online_configuration)
                 self.conf_man.save_online_configuration_local(
@@ -157,7 +151,6 @@ class DataLogger:
                 self.packet_manager.update_configuration()
         except:
             self.logger.warning('Problem updating configuration')
-            raise
         try:
             self.scheduler.unschedule_func(
                 self.load_online_configuration_and_initiate_sending_data)
